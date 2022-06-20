c472bfdfd5d27be8a20def5c235c7a20d845bca8
Ciahaka <wilddiver.by@gmail.com>
2022-06-21 01:26:23 +0300

    add CssSamurai

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='site-wrapper'>
+      <header className='header'>
+        <img src="https://yt3.ggpht.com/ytc/AKedOLQInOfZQfNNUXTiEcPL2wC232E6jWFx1boiWaWc=s900-c-k-c0x00ffffff-no-rj"
+             alt="Кусто, шарж"/>
+      </header>
+
+      <nav className='nav'>
+
+        <ul>
+          <li>Profile</li>
+          <li>Message</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+      <div className='content'>
+        <div><img
+          src="https://m.media-amazon.com/images/M/MV5BM2U4YzhmNWItZDM3ZS00MWViLWI0ZjItNmZmMjM0MTVhNmEzXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_.jpg"
+          alt="Подводник стоит на дне перед челюстями монстра" width="695px"
+          height="354px"/>
+        </div>
+        <div>Ava+description</div>
+        <div>My posts
+          <div>
+            New posts
+          </div>
+          <ul>
+            <li>Post 1</li>
+            <li>Post 2</li>
+            <li>Post 3</li>
+          </ul>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
