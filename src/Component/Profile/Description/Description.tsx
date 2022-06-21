145d4484af2c0cc9ae5620d4536c6982b62e324d
Ciahaka <wilddiver.by@gmail.com>
2022-06-22 02:29:05 +0300

    add Descr.tsx

---
+++ src/Component/Profile/Description/Description.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from "./Description.module.css";
+
+export const Description = () => {
+  return (
+    <div className={classes.content}>
+      <h1> Лявон Падводны</h1>
+
+
+    </div>
+  )
+
+}
