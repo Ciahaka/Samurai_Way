b9af4ff4e51fa66abda74aa89acc901fbeed7894
Ciahaka <wilddiver.by@gmail.com>
2022-06-22 03:04:18 +0300

    change Description.tsx

--- src/Component/Profile/Description/Description.tsx
+++ src/Component/Profile/Description/Description.tsx
@@ -5,7 +5,14 @@ export const Description = () => {
   return (
     <div className={classes.content}>
       <h1> Лявон Падводны</h1>
-
+      <h3>
+        <div>Дата рождения: 30 февраля</div>
+       <div> Город: Атлантида</div>
+       <div>Образование: Водолазная школа</div>
+       <div>
+         WebSite: <a href={'https://ichthyander.lol'}> сthulhu.lol </a>
+       </div>
+      </h3>
 
     </div>
   )
