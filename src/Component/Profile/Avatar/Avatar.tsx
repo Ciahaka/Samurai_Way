b4e3d25f4fe22a360003179466605cf3e6194704
Ciahaka <wilddiver.by@gmail.com>
2022-06-22 03:04:30 +0300

    change Avatar.tsx

--- src/Component/Profile/Avatar/Avatar.tsx
+++ src/Component/Profile/Avatar/Avatar.tsx
@@ -4,8 +4,11 @@ import classes from "./Avatar.module.css";
 export const Avatar = () => {
   return (
     <div className={classes.content}>
+
       <img src="https://kartinkin.net/uploads/posts/2021-07/1625176910_34-kartinkin-com-p-vodolaz-art-art-krasivo-35.jpg" alt="Арт водолаз обнимает фридайвера"/>
+
     </div>
+
   )
 
 }
