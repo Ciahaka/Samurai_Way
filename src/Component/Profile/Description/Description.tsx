00b9907d969d7d0977e3dd1c63df994ea67db17f
Ciahaka <wilddiver.by@gmail.com>
2022-06-22 23:36:16 +0300

    change Description.tsx

--- src/Component/Profile/Description/Description.tsx
+++ src/Component/Profile/Description/Description.tsx
@@ -3,18 +3,16 @@ import classes from "./Description.module.css";
 
 export const Description = () => {
   return (
-    <div className={classes.content}>
-      <h1> Лявон Падводны</h1>
-      <h3>
-        <div>Дата рождения: 30 февраля</div>
-       <div> Город: Атлантида</div>
-       <div>Образование: Водолазная школа</div>
-       <div>
-         WebSite: <a href={'https://ichthyander.lol'}> сthulhu.lol </a>
-       </div>
-      </h3>
+      <div className={classes.desc} >
+        <h1 className={classes.h1}> Лявон Падводны</h1>
+        <h3>
+          <div>Дата рождения: 30 февраля</div>
+          <div> Город: Атлантида</div>
+          <div>Образование: Водолазная школа</div>
+          WebSite: <a className={classes.a} href={'https://ichthyander.lol'}> сthulhu.lol </a>
+        </h3>
+      </div>
 
-    </div>
   )
 
 }
