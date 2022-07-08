c622cbb2a5cf5cd6961a067efc9cdb0c5ecd1daa
Ciahaka <wilddiver.by@gmail.com>
2022-07-08 23:55:42 +0300

    add News_19

--- src/Component/Nav/Nav.tsx
+++ src/Component/Nav/Nav.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import classes  from '../Nav/Nav.module.css'
+import classes from '../Nav/Nav.module.css'
 
 export const Nav = () => {
   return (
     <nav className={classes.nav}>
 
       <ul className={classes.item}>
-        <li>Profile</li>
-        <li>Message</li>
-        <li className={`${classes.item} ${classes.active}`}>News</li>
-        <li>Music</li>
-        <li>Settings</li>
+        <li>
+          <a href="/profile">Profile</a>
+        </li>
+        <li>
+          <a href="/dialogs">Message</a>
+        </li>
+        <li className={`${classes.item} ${classes.active}`}>
+          <a href="/news" >News</a>
+        </li>
+        <li>
+          <a href="/music">Music</a>
+        </li>
+        <li>
+          <a href="/settings">Settings</a>
+        </li>
       </ul>
     </nav>
   )
