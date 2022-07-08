a55d8957a545656c3ae5a1e3c426e9fcf03740f5
Ciahaka <wilddiver.by@gmail.com>
2022-07-08 23:54:09 +0300

    change App.tsx_19

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,29 @@ import './App.css';
 import {Header} from "./Component/Header/Header";
 import {Nav} from "./Component/Nav/Nav";
 import {Profile} from "./Component/Profile/Profile";
+import {Dialogs} from "./Component/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./Component/News/News";
+import {Music} from "./Component/Music/Music";
+import {Settings} from "./Component/Setting/Setting";
+
 
 export const App = () => {
   return (
-    <div className='site-wrapper'>
-      <Header/>
-      <Nav/>
-      <Profile/>
-    </div>
+    <BrowserRouter>
+      <div className='siteWrapper'>
+        <Header/>
+        <Nav/>
+        <div className='siteWrapperContent'>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
