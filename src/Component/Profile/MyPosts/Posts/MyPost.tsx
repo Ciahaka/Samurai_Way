15acbd6b732dd01a458a4dc1c6513c6e3ef6a9a0
Ciahaka <wilddiver.by@gmail.com>
2022-07-10 00:20:55 +0300

    change MyPost_21

--- src/Component/Profile/MyPosts/Posts/MyPost.tsx
+++ src/Component/Profile/MyPosts/Posts/MyPost.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import classes from "./MyPost.module.css";
 import {MyMessage} from "./Messages/MyMessage";
-import {ButtonSend} from "../ButtonSend/ButtonSend";
-import {InputArea} from "../InputArea/InputArea";
+import {ButtonSend} from "../../PersonalPage/ButtonSend/ButtonSend";
+import {InputArea} from "../../PersonalPage/InputArea/InputArea";
 
 
 
 export const MyPost = () => {
   return (
     <div>
-      <InputArea/>
-      <ButtonSend/>
       <MyMessage message={'Как дела?'} likeCount={15} />
       <MyMessage message={'Так себе, братка!'} likeCount={2}/>
       <MyMessage message={'Что так??'} likeCount={3}/>
