8e28c36cb9de639dd441bf38bc359a9ddf1adc69
Ciahaka <wilddiver.by@gmail.com>
2022-07-10 00:21:11 +0300

    change Profile_21

--- src/Component/Profile/Profile.tsx
+++ src/Component/Profile/Profile.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import classes from "../Profile/Profile.module.css";
 import {MyPost} from "./MyPosts/Posts/MyPost";
-import {BackImage} from "./BackImg/BackImage";
-import {Avatar} from "./Avatar/Avatar";
-import {Description} from "./Description/Description";
+import {BackImage} from "./PersonalPage/BackImg/BackImage";
+import {Avatar} from "./PersonalPage/Avatar/Avatar";
+import {Description} from "./PersonalPage/Description/Description";
+import {ButtonSend} from "./PersonalPage/ButtonSend/ButtonSend";
+import {InputArea} from "./PersonalPage/InputArea/InputArea";
 
 
 export const Profile = () => {
   return (
-    <div>
+    <div className={classes.profile}>
       <BackImage/>
       <Avatar/>
-      <Description/>
+      <Description />
+      <InputArea/>
+      <ButtonSend/>
       <MyPost/>
     </div>
   )
