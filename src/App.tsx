dcfd1bcf3e534526e7ee41887f85f24cece04820
Ciahaka <wilddiver.by@gmail.com>
2023-02-17 19:33:34 +0300

    reprint all

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from "react-router-dom";
-import {Header} from "./Component/Header/Header";
-import {Nav} from "./Component/Nav/Nav";
-import {Profile} from "./Component/Profile/Profile";
-import {Dialogs} from "./Component/Dialogs/Dialogs";
-import {News} from "./Component/News/News";
-import {Music} from "./Component/Music/Music";
-import {Settings} from "./Component/Setting/Setting";
+
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <div className='siteWrapper'>
-        <Header/>
-        <Nav/>
-        <div className='siteWrapperContent'>
-          <Route path={'/dialogs'} render={Dialogs}/>
-          <Route path={'/profile'} render={Profile}/>
-          <Route path={'/news'} render={News}/>
-          <Route path={'/music'} render={Music}/>
-          <Route path={'/settings'} render={Settings}/>
+        <div >
 
         </div>
-      </div>
-    </BrowserRouter>
+
+
   );
 }
 
--- src/Component/Profile/PersonalPage/ButtonSend/ButtonSend.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes  from "./buttonSend.module.css";
-
-
-export const ButtonSend = () => {
-  return (<button className={classes.btn}>Add Post</button>)
-
-}
