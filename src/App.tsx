81463eaccda6ef6c55b8d9be9f4628f227fbc974
Ciahaka <wilddiver.by@gmail.com>
2023-02-21 00:21:19 +0300

    finish class #15 Samurai

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {Header} from './Component/header/Header';
 import {Footer} from './Component/footer/Footer';
 import {NavBar} from './Component/navigation aside/NavBar';
-import {Profile} from './Component/main content/Profile';
+import {Profile} from './Component/profile content/profile/Profile';
 import {NewsBar} from './Component/news aside/NewsBar';
 
 export const App = () => {
