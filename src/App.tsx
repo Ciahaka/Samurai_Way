fa197e4113359bf27c2638c6ca764cc404939c4a
Ciahaka <wilddiver.by@gmail.com>
2023-02-21 23:45:48 +0300

    finish class #18 Samurai(Dialogs)

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import {Footer} from './Component/footer/Footer';
 import {NavBar} from './Component/navigation aside/NavBar';
 import {Profile} from './Component/profile content/profile/Profile';
 import {NewsBar} from './Component/news aside/NewsBar';
+import {Dialogs} from './Component/dialogs/Dialogs';
 
 export const App = () => {
   return (
     <div className="app-wrapper">
       <Header/>
       <NavBar/>
-      <Profile/>
+      <div className="app-wrapper-content">
+        {/*<Profile/>*/}
+        <Dialogs/>
+      </div>
       <NewsBar/>
       <Footer/>
     </div>
