bfe6b1120b163a2320802e443dd1a51dc36c6e8e
Ciahaka <wilddiver.by@gmail.com>
2023-02-21 22:03:35 +0300

    finish class #16 Samurai (Props)

--- src/Component/profile content/my posts/MyPosts.tsx
+++ src/Component/profile content/my posts/MyPosts.tsx
@@ -8,9 +8,9 @@ export const MyPosts = () => {
       <h3>My posts</h3>
       <textarea></textarea>
       <button> add New Post </button>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post message ={'Привет! Что нового?!'} likesCount={15}/>
+      <Post message ={'Всё отлично! Как сам?'} likesCount={3}/>
+      <Post message ={'В порядке! Спасибо зарядке!'} likesCount={8}/>
     </div>
   )
 }
--- src/Component/profile content/single post/Post.tsx
+++ src/Component/profile content/single post/Post.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import s from './Post.module.css'
 import shit from '../../../multimedia/images/satan-shit.svg'
 
-export const Post = () => {
-  return (
 
+export type PostPropsType = {
+  message:string
+  likesCount:number
+}
+export const Post = (props:PostPropsType) => {
+  const {message, likesCount}=props
+  return (
     <div>
       <img className={s.avatarImage} src={shit} alt="shit icon"/>
-      <p>post</p>
-      <span>like</span>
+      <p>{message}</p>
+      <span>{likesCount}</span>
     </div>
 
   )
