aa4c0a602d3c38ec82540a72601d3e9826a861b5
Ciahaka <wilddiver.by@gmail.com>
2023-02-21 23:42:12 +0300

    refactor Post component

--- src/Component/profile content/single post/Post.tsx
+++ src/Component/profile content/single post/Post.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import s from './Post.module.css'
 import shit from '../../../multimedia/images/satan-shit.svg'
-
+import like from '../../../multimedia/images/fuck.svg'
 
 export type PostPropsType = {
-  message:string
-  likesCount:number
+  message: string
+  likesCount: number
 }
-export const Post = (props:PostPropsType) => {
-  const {message, likesCount}=props
+export const Post = (props: PostPropsType) => {
+  const {message, likesCount} = props
   return (
-    <div>
+    <div >
       <img className={s.avatarImage} src={shit} alt="shit icon"/>
-      <p>{message}</p>
-      <span>{likesCount}</span>
+      {message}
+        <div className={s.span}>
+          {likesCount}
+          <img className={s.likeIcon} src={like} alt="like icon"/>
+        </div>
+
     </div>
 
   )
