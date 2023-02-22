6a2cc555b03c1008b9ec1771c9d8823ed5958dde
Ciahaka <wilddiver.by@gmail.com>
2023-02-22 18:08:25 +0300

    change component Dialog

--- src/Component/navigation aside/dialogs/Dialogs.tsx
+++ src/Component/navigation aside/dialogs/Dialogs.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-
+import {NavLink} from 'react-router-dom';
 
 
 export const Dialogs = () => {
-  return <div className={s.dialogs} >
+  return <div className={s.dialogs}>
     <div className={s.dialogsItems}>
-      <div className={s.singleDialog}>Oleg</div>
-      <div className={s.singleDialog}>Turbo</div>
-      <div className={s.singleDialog}>Morgan</div>
-      <div className={s.singleDialog}>Vadim</div>
-      <div className={s.singleDialog}>Алексей</div>
-      <div className={s.singleDialog}>ХАН</div>
+      <div className={s.singleDialog}>
+        <NavLink to={'/Dialogs/1'} rel={'no-referrer nofollow no-opener'}
+                 style={(params) => {
+                   return {color: params.isActive ? 'darkred' : 'darkgreen'}
+                 }}>
+          Oleg
+        </NavLink>
+      </div>
+      <div className={s.singleDialog}>
+        <NavLink to={'/Dialogs/2'} rel={'no-referrer nofollow no-opener'} style={(params) => {
+          return {color: params.isActive ? 'darkred' : 'darkgreen'}
+        }}>
+          Turbo
+        </NavLink>
+      </div>
+      <div className={s.singleDialog}>
+        <NavLink to={'/Dialogs/3'} rel={'no-referrer nofollow no-opener'} style={(params) => {
+          return {color: params.isActive ? 'darkred' : 'darkgreen'}
+        }}>
+          Morgan
+        </NavLink>
+      </div>
+      <div className={s.singleDialog}>
+        <NavLink to={'/Dialogs/4'} rel={'no-referrer nofollow no-opener'} style={(params) => {
+          return {color: params.isActive ? 'darkred' : 'darkgreen'}
+        }}>
+          Vadim
+        </NavLink>
+      </div>
+      <div className={s.singleDialog}>
+        <NavLink to={'/Dialogs/5'} rel={'no-referrer nofollow no-opener'} style={(params) => {
+          return {color: params.isActive ? 'darkred' : 'darkgreen'}
+        }}>
+          Алексей
+        </NavLink>
+      </div>
+      <div className={s.singleDialog}>
+        <NavLink to={'/Dialogs/6'} rel={'no-referrer nofollow no-opener'} style={(params) => {
+          return {color: params.isActive ? 'darkred' : 'darkgreen'}
+        }}>
+          ХАН
+        </NavLink>
+      </div>
     </div>
     <div className={s.messagesItems}>
       <div className={s.singleMessages}>Привет! Рады видеть тебя здесь!</div>
