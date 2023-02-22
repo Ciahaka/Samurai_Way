4103c1d7c3f05dd67fcb812ccd3fc53bdc373976
Ciahaka <wilddiver.by@gmail.com>
2023-02-22 22:21:00 +0300

    refactor Single Dialog component

---
+++ src/Component/navigation aside/dialogs block/single dialog/SingleDialog.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './SingleDialog.module.css'
+import {NavLink} from 'react-router-dom';
+
+export type SingleDialogPropsType = {
+  id:string
+  name:string
+}
+export const SingleDialog =(props:SingleDialogPropsType) =>{
+  const{id,name} = props
+  return (
+    <div className={s.singleDialog}>
+      <NavLink to={'/Dialogs/' + id} rel={'no-referrer nofollow no-opener'}
+               style={(params) => {
+                 return {color: params.isActive ? 'darkred' : 'darkgreen'}
+               }}>
+        {name}
+      </NavLink>
+    </div>
+
+  )
+}
