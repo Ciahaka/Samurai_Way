a395463903c44e3f761bb4609b302f9a147fc3fc
Ciahaka <wilddiver.by@gmail.com>
2023-02-23 16:32:17 +0300

    refactor Dialogs component

--- src/Component/navigation aside/dialogs block/dialogs/Dialogs.tsx
+++ src/Component/navigation aside/dialogs block/dialogs/Dialogs.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-import {NavLink} from 'react-router-dom';
 import {SingleDialog} from '../single dialog/SingleDialog';
 import {SingleMessage} from '../single message/SingleMessage';
 
 
 export const Dialogs = () => {
+  const dialogsData = [
+    {id:'1', name:'Oleg'},
+    {id:'2', name:'Turbo'},
+    {id:'3', name:'Morgan'},
+    {id:'4', name:'Vadim'},
+    {id:'5', name:'Алексей'},
+    {id:'6', name:'ХАН'},
+  ]
+  const messageData = [
+    {id:'1', message:'Привет! Рады видеть тебя здесь!'},
+    {id:'2', message:'Спасибо за приглашение!'},
+    {id:'3', message:'Hi bro'},
+  ]
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <SingleDialog id={'1'} name={'Oleg'}/>
-        <SingleDialog id={'2'} name={'Turbo'}/>
-        <SingleDialog id={'3'} name={'Morgan'}/>
-        <SingleDialog id={'4'} name={'Vadim'}/>
-        <SingleDialog id={'5'} name={'Алексей'}/>
-        <SingleDialog id={'6'} name={'ХАН'}/>
+        <SingleDialog id={dialogsData[0].id} name={dialogsData[0].name}/>
+        <SingleDialog id={dialogsData[1].id} name={dialogsData[1].name}/>
+        <SingleDialog id={dialogsData[2].id} name={dialogsData[2].name}/>
+        <SingleDialog id={dialogsData[3].id} name={dialogsData[3].name}/>
+        <SingleDialog id={dialogsData[4].id} name={dialogsData[4].name}/>
+        <SingleDialog id={dialogsData[5].id} name={dialogsData[5].name}/>
       </div>
+
       <div className={s.messagesItems}>
-        <SingleMessage message={'Привет! Рады видеть тебя здесь!'}/>
-        <SingleMessage message={'Спасибо за приглашение!'}/>
-        <SingleMessage message={'Hi bro'}/>
+        <SingleMessage id={messageData[0].id} message={messageData[0].message}/>
+        <SingleMessage id={messageData[0].id} message={messageData[0].message}/>
+        <SingleMessage id={messageData[0].id} message={messageData[0].message}/>
       </div>
     </div>
   )
