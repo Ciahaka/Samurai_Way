3a42e999fb78721072ce16d29b34d5f91b1df088
Ciahaka <wilddiver.by@gmail.com>
2023-02-23 16:33:48 +0300

    finish class #24 Samurai(UI and BLL)

--- src/Component/navigation aside/dialogs block/single message/SingleMessage.tsx
+++ src/Component/navigation aside/dialogs block/single message/SingleMessage.tsx
@@ -3,6 +3,7 @@ import s from './SingleMessage.module.css'
 
 
 export type SingleMessagePropsType = {
+  id: string
   message: string
 }
 export const SingleMessage = (props: SingleMessagePropsType) => {
