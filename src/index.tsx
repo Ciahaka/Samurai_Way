cbb20a89edce19ced5109cba8a40f15e5743f327
Ciahaka <wilddiver.by@gmail.com>
2023-02-24 00:03:37 +0300

    finish class #23 Samurai(BLL into index)

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,29 @@ import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+export const dialogsData = [
+  {id: '1', name: 'Oleg'},
+  {id: '2', name: 'Turbo'},
+  {id: '3', name: 'Morgan'},
+  {id: '4', name: 'Vadim'},
+  {id: '5', name: 'Алексей'},
+  {id: '6', name: 'ХАН'},
+]
+export const messageData = [
+  {id: '1', message: 'Привет! Рады видеть тебя здесь!'},
+  {id: '2', message: 'Спасибо за приглашение!'},
+  {id: '3', message: 'Hi bro'},
+]
+
+const postsData = [
+  {id: 1, message: 'Привет! Что нового?!', likesCount: 15},
+  {id: 2, message: 'Всё отлично! Как сам?', likesCount: 3},
+  {id: 3, message: 'В порядке! Спасибо зарядке!', likesCount: 8},
+]
 root.render(
   <BrowserRouter>
-    <App/>
+    <App dialogsData={dialogsData} messageData={messageData} postsData={postsData} />
   </BrowserRouter>
 )
 reportWebVitals();
