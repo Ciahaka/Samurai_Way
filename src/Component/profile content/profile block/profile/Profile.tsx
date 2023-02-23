2c13ac56f02d0cfdbaa46bbb597a4a95fdaf76eb
Ciahaka <wilddiver.by@gmail.com>
2023-02-24 00:00:45 +0300

    props to Profile

--- src/Component/profile content/profile block/profile/Profile.tsx
+++ src/Component/profile content/profile block/profile/Profile.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import s from './Profile.module.css'
 import {MyPosts} from '../../posts block/my posts/MyPosts';
 import {ProfileInfo} from '../profile info/ProfileInfo';
+import {PostsType} from '../../../../App';
 
-export const Profile = () => {
+
+export type ProfilePropsType = {
+  postsData: PostsType[]
+}
+export const Profile = (props:ProfilePropsType) => {
   return (
     <div className={s.section}>
       <section>
         <h1 className={s.h1}>Main Page</h1>
-        <ProfileInfo/>
-        <MyPosts/>
+        <ProfileInfo />
+        <MyPosts postsData={props.postsData}/>
 
       </section>
     </div>
