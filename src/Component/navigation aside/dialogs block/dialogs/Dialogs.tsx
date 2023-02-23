e6d12c578540e454f2019722292e641a33485010
Ciahaka <wilddiver.by@gmail.com>
2023-02-23 17:23:10 +0300

    finish class #25 Samurai(use .map)

--- src/Component/navigation aside/dialogs block/dialogs/Dialogs.tsx
+++ src/Component/navigation aside/dialogs block/dialogs/Dialogs.tsx
@@ -6,34 +6,30 @@ import {SingleMessage} from '../single message/SingleMessage';
 
 export const Dialogs = () => {
   const dialogsData = [
-    {id:'1', name:'Oleg'},
-    {id:'2', name:'Turbo'},
-    {id:'3', name:'Morgan'},
-    {id:'4', name:'Vadim'},
-    {id:'5', name:'Алексей'},
-    {id:'6', name:'ХАН'},
+    {id: '1', name: 'Oleg'},
+    {id: '2', name: 'Turbo'},
+    {id: '3', name: 'Morgan'},
+    {id: '4', name: 'Vadim'},
+    {id: '5', name: 'Алексей'},
+    {id: '6', name: 'ХАН'},
   ]
   const messageData = [
-    {id:'1', message:'Привет! Рады видеть тебя здесь!'},
-    {id:'2', message:'Спасибо за приглашение!'},
-    {id:'3', message:'Hi bro'},
+    {id: '1', message: 'Привет! Рады видеть тебя здесь!'},
+    {id: '2', message: 'Спасибо за приглашение!'},
+    {id: '3', message: 'Hi bro'},
   ]
+
+  const dialogsElement = dialogsData.map((d) => <SingleDialog id={d.id} name={d.name}/>)
+  const messagesElement = messageData.map(m => <SingleMessage id={m.id} message={m.message}></SingleMessage>)
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <SingleDialog id={dialogsData[0].id} name={dialogsData[0].name}/>
-        <SingleDialog id={dialogsData[1].id} name={dialogsData[1].name}/>
-        <SingleDialog id={dialogsData[2].id} name={dialogsData[2].name}/>
-        <SingleDialog id={dialogsData[3].id} name={dialogsData[3].name}/>
-        <SingleDialog id={dialogsData[4].id} name={dialogsData[4].name}/>
-        <SingleDialog id={dialogsData[5].id} name={dialogsData[5].name}/>
+        {dialogsElement}
       </div>
 
       <div className={s.messagesItems}>
-        <SingleMessage id={messageData[0].id} message={messageData[0].message}/>
-        <SingleMessage id={messageData[0].id} message={messageData[0].message}/>
-        <SingleMessage id={messageData[0].id} message={messageData[0].message}/>
+        {messagesElement}
       </div>
     </div>
   )
--- src/Component/profile content/posts block/my posts/MyPosts.tsx
+++ src/Component/profile content/posts block/my posts/MyPosts.tsx
@@ -8,6 +8,7 @@ export const MyPosts = () => {
     {id: 2, message: 'Всё отлично! Как сам?', likesCount: 3},
     {id: 3, message: 'В порядке! Спасибо зарядке!', likesCount: 8},
   ]
+  const postsElement = postsData.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
   return (
     <div className={s.posts}>
       <h3>My posts</h3>
@@ -17,10 +18,7 @@ export const MyPosts = () => {
       <div className={s.btnArea}>
         <button> add New Post</button>
       </div>
-      <Post id={postsData[0].id} message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-      <Post id={postsData[1].id} message={postsData[1].message} likesCount={postsData[1].likesCount}/>
-      <Post id={postsData[2].id} message={postsData[2].message} likesCount={postsData[2].likesCount}/>
-
+      {postsElement}
     </div>
   )
 }
