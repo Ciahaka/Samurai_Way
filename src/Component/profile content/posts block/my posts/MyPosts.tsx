98b490c1ce01378564c64e4f9b85a6585657da0d
Ciahaka <wilddiver.by@gmail.com>
2023-02-23 16:32:34 +0300

    refactor MyPosts component

--- src/Component/profile content/posts block/my posts/MyPosts.tsx
+++ src/Component/profile content/posts block/my posts/MyPosts.tsx
@@ -3,6 +3,11 @@ import s from './MyPosts.module.css'
 import {Post} from '../../single post/Post';
 
 export const MyPosts = () => {
+  const postsData = [
+    {id: 1, message: 'Привет! Что нового?!', likesCount: 15},
+    {id: 2, message: 'Всё отлично! Как сам?', likesCount: 3},
+    {id: 3, message: 'В порядке! Спасибо зарядке!', likesCount: 8},
+  ]
   return (
     <div className={s.posts}>
       <h3>My posts</h3>
@@ -12,9 +17,10 @@ export const MyPosts = () => {
       <div className={s.btnArea}>
         <button> add New Post</button>
       </div>
-      <Post message={'Привет! Что нового?!'} likesCount={15}/>
-      <Post message={'Всё отлично! Как сам?'} likesCount={3}/>
-      <Post message={'В порядке! Спасибо зарядке!'} likesCount={8}/>
+      <Post id={postsData[0].id} message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+      <Post id={postsData[1].id} message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+      <Post id={postsData[2].id} message={postsData[2].message} likesCount={postsData[2].likesCount}/>
+
     </div>
   )
 }
