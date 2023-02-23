57a02edeb7227a35c843773cb36db2d63388b22f
Ciahaka <wilddiver.by@gmail.com>
2023-02-24 00:01:10 +0300

    props to MyPosts

--- src/Component/profile content/posts block/my posts/MyPosts.tsx
+++ src/Component/profile content/posts block/my posts/MyPosts.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import {Post} from '../../single post/Post';
+import {PostsType} from '../../../../App';
 
-export const MyPosts = () => {
-  const postsData = [
-    {id: 1, message: 'Привет! Что нового?!', likesCount: 15},
-    {id: 2, message: 'Всё отлично! Как сам?', likesCount: 3},
-    {id: 3, message: 'В порядке! Спасибо зарядке!', likesCount: 8},
-  ]
-  const postsElement = postsData.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
+export type PostPropsType = {
+  postsData: PostsType[]
+}
+
+export const MyPosts = (props:PostPropsType) => {
+
+  const postsElement = props.postsData.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
   return (
     <div className={s.posts}>
       <h3>My posts</h3>
