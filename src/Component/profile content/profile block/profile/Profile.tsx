0e22d6ae2319f41ef65f978eea78cc9dca53c88d
Ciahaka <wilddiver.by@gmail.com>
2023-02-25 00:43:12 +0300

    refactor Profile component

--- src/Component/profile content/profile block/profile/Profile.tsx
+++ src/Component/profile content/profile block/profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Profile.module.css'
 import {MyPosts} from '../../posts block/my posts/MyPosts';
 import {ProfileInfo} from '../profile info/ProfileInfo';
-import {PostDataType} from '../../../../state/state';
+import {PostDataType} from '../../../../redux/state';
 
 
 
