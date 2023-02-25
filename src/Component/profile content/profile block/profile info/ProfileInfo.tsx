6584734cacbe6caf324a46e9aef118729a55a5f0
Ciahaka <wilddiver.by@gmail.com>
2023-02-25 22:01:27 +0300

    refactor ProfileInfo component

--- src/Component/profile content/profile block/profile info/ProfileInfo.tsx
+++ src/Component/profile content/profile block/profile info/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import starry_sky from '../../../../multimedia/images/img_for_main_content.jpg'
+import luka from '../../../../multimedia/images/lukashenko.svg'
 
 
 export const ProfileInfo = () => {
@@ -9,8 +10,18 @@ export const ProfileInfo = () => {
       <div>
         <img className={s.imgBack} src={starry_sky} alt="starry sky haze"/>
       </div>
-      <div>
-        <h3>Ava + Description</h3>
+      <div className={s.profileBlock}>
+        <h3>Profile Info</h3>
+        <div className={s.infoBlock}>
+          <img className={s.avaIcon} src={luka} alt="luka icon"/>
+          <ul >
+            <li>Profile Name: ХАН</li>
+            <li>Age Old: 37</li>
+            <li>Male: Man</li>
+            <li>Interests:I love Spitz dogs </li>
+          </ul>
+        </div>
+
       </div>
     </div>
 
