7e39f7833889d634e86b51514dc22e52b5f72dad
Ciahaka <wilddiver.by@gmail.com>
2023-02-25 22:00:35 +0300

    refactor NavBar component

--- src/Component/navigation aside/navbar/NavBar.tsx
+++ src/Component/navigation aside/navbar/NavBar.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import s from './NavBar.module.css'
 import {NavLink} from 'react-router-dom';
+import {Friends} from '../friends/Friends';
 
 
 export const NavBar = () => {
   return (
     <div className={s.navAside}>
       <aside className={s.aside}>
-        <NavLink to={'/Profile'} rel={'no-referrer nofollow no-opener'}
-                 style={(params) => {
-                   return {color: params.isActive ? 'darkred' : 'darkgreen'}
-                 }}> Profile </NavLink>
-        <NavLink  to={'/Dialogs'} rel={'no-referrer nofollow no-opener'}
-                 style={(params) => {
-          return {color: params.isActive ? 'darkred' : 'darkviolet'}
-        }}> Dialogs</NavLink>
-        <NavLink to={'/Music'} rel={'no-referrer nofollow no-opener'}
-                 style={(params) => {
-          return {color: params.isActive ? 'darkred' : 'darkgreen'}
-        }}>Music</NavLink>
-        <NavLink to={'/Photo'} rel={'no-referrer nofollow no-opener'}
-                 style={(params) => {
-          return {color: params.isActive ? 'darkred' : 'darkviolet'}
-        }}>Photo</NavLink>
-        <NavLink to={'/News'} rel={'no-referrer nofollow no-opener'}
-                 style={(params) => {
-          return {color: params.isActive ? 'darkred' : 'darkgreen'}
-        }}>News</NavLink>
+        <div className={s.navItem}>
+          <NavLink to={'/Profile'} rel={'no-referrer nofollow no-opener'}
+                   style={(params) => {
+                     return {color: params.isActive ? 'darkred' : 'darkgreen'}
+                   }}> Profile </NavLink>
+          <NavLink to={'/Dialogs'} rel={'no-referrer nofollow no-opener'}
+                   style={(params) => {
+                     return {color: params.isActive ? 'darkred' : 'darkviolet'}
+                   }}> Dialogs</NavLink>
+          <NavLink to={'/Music'} rel={'no-referrer nofollow no-opener'}
+                   style={(params) => {
+                     return {color: params.isActive ? 'darkred' : 'darkgreen'}
+                   }}>Music</NavLink>
+          <NavLink to={'/Photo'} rel={'no-referrer nofollow no-opener'}
+                   style={(params) => {
+                     return {color: params.isActive ? 'darkred' : 'darkviolet'}
+                   }}>Photo</NavLink>
+        </div>
+        <div className={s.navItem}>
+          <NavLink to={'/Friends'} rel={'no-referrer nofollow no-opener'}
+                   style={(params) => {
+                     return {color: params.isActive ? 'darkred' : 'darkorange'}
+                   }}>Friends</NavLink>
+          <Friends/>
+        </div>
+
       </aside>
     </div>
   )
