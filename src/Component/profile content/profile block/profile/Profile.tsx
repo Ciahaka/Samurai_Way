166a75c91490be0ab918109998bc1dc65f296da3
Ciahaka <wilddiver.by@gmail.com>
2023-02-25 22:01:15 +0300

    refactor Profile component

--- src/Component/profile content/profile block/profile/Profile.tsx
+++ src/Component/profile content/profile block/profile/Profile.tsx
@@ -3,21 +3,23 @@ import s from './Profile.module.css'
 import {MyPosts} from '../../posts block/my posts/MyPosts';
 import {ProfileInfo} from '../profile info/ProfileInfo';
 import {PostDataType} from '../../../../redux/state';
+import {OpponentPost} from '../../opponent post/OpponentPost';
 
 
 
 
 export type ProfilePropsType = {
   postsData: PostDataType[]
+
 }
 export const Profile = (props: ProfilePropsType) => {
   return (
-    <div className={s.section}>
-      <section>
-        <h1 className={s.h1}>Main Page</h1>
+    <div className={s.profile}>
+      <h2 >Main Page</h2>
+      <section >
         <ProfileInfo/>
         <MyPosts postsData={props.postsData}/>
-
+        <OpponentPost id={1} message={'Как же мне это одолеть??'} likesCount={10}/>
       </section>
     </div>
   )
