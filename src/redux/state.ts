54c0ee68cf468aed0362632179278290f1995532
Ciahaka <wilddiver.by@gmail.com>
2023-03-04 15:06:46 +0300

    add func addPostsMessage into BLL

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,3 +1,5 @@
+import {v1} from 'uuid';
+
 export type DialogDataType = {
   id: string
   name: string
@@ -7,7 +9,7 @@ export type MessageDataType = {
   message: string
 }
 export type PostDataType = {
-  id: number
+  id: string
   message: string
   likesCount: number
 }
@@ -16,37 +18,48 @@ export type DialogsType = {
   messageData: MessageDataType[]
 }
 export type PostsType = {
-  postsData:PostDataType[]
+  postsData: PostDataType[]
 
 }
 export type RootStateType = {
-  dialogs:DialogsType
-  posts:PostsType
+  dialogs: DialogsType
+  posts: PostsType
 
 }
-export const rootState:RootStateType = {
+export const rootState: RootStateType = {
   dialogs: {
     dialogsData: [
-      {id: '1', name: 'Oleg'},
-      {id: '2', name: 'Turbo'},
-      {id: '3', name: 'Morgan'},
-      {id: '4', name: 'Vadim'},
-      {id: '5', name: 'Алексей'},
-      {id: '6', name: 'ХАН'},
+      {id: v1(), name: 'Oleg'},
+      {id: v1(), name: 'Turbo'},
+      {id: v1(), name: 'Morgan'},
+      {id: v1(), name: 'Vadim'},
+      {id: v1(), name: 'Алексей'},
+      {id: v1(), name: 'ХАН'},
     ],
     messageData: [
-      {id: '1', message: 'Привет! Рады видеть тебя здесь!'},
-      {id: '2', message: 'Спасибо за приглашение!'},
-      {id: '3', message: 'Hi bro'},
+      {id: v1(), message: 'Привет! Рады видеть тебя здесь!'},
+      {id: v1(), message: 'Спасибо за приглашение!'},
+      {id: v1(), message: 'Hi bro'},
     ],
   },
   posts: {
     postsData: [
-      {id: 1, message: 'Привет! Что нового?!', likesCount: 15},
-      {id: 2, message: 'Всё отлично! Как сам?', likesCount: 3},
-      {id: 3, message: 'В порядке! Спасибо зарядке!', likesCount: 8},
+      {id: v1(), message: 'Привет! Что нового?!', likesCount: 15},
+      {id: v1(), message: 'Всё отлично! Как сам?', likesCount: 3},
+      {id: v1(), message: 'В порядке! Спасибо зарядке!', likesCount: 8},
     ]
   },
+
+}
+
+export const addPostsMessage = (postMessage: string) => {
+  const newPostsMessage = {
+    id: v1(),
+    message: postMessage,
+    likesCount: 145
+  };
+
+  rootState.posts.postsData.push(newPostsMessage)
 
 }
 
