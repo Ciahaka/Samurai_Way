a18d20c6d8bbbcd35a55e2324a3a5bbf3cce5936
Ciahaka <wilddiver.by@gmail.com>
2023-03-04 17:37:04 +0300

    refactor index

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {bllSetRender} from './render';
+import {rootState} from './redux/state';
 
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-
-
-root.render(
-  <BrowserRouter>
-    <App
-    />
-  </BrowserRouter>
-)
-reportWebVitals();
+bllSetRender(rootState)
