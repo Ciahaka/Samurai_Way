927129f5744bc31874241b12e631f2adea0eebe5
Ciahaka <wilddiver.by@gmail.com>
2023-03-04 23:17:56 +0300

    refactor MyPosts

--- src/Component/profile content/posts block/my posts/MyPosts.tsx
+++ src/Component/profile content/posts block/my posts/MyPosts.tsx
@@ -8,7 +8,9 @@ import {MessageButton} from '../../../button message/MessageButton';
 
 export type PostPropsType = {
   postsData: PostDataType[]
-  addPostsMessage:(postMessage:string)=>void
+  addPostsMessage: () => void
+  textForUpdate: string
+  updateTextPostsMessage: (newText: string) => void
 }
 
 export const MyPosts = (props: PostPropsType) => {
@@ -17,20 +19,25 @@ export const MyPosts = (props: PostPropsType) => {
                                                       message={p.message}
                                                       likesCount={p.likesCount}/>)
   const newPostElement = useRef<HTMLTextAreaElement>(null)
-  const addNewPost = ()=>{
-    if (newPostElement.current){
-      props.addPostsMessage(newPostElement.current.value)
-      newPostElement.current.value=''
-    }
+  const addNewPostHandler = () => {
+    props.addPostsMessage()
   }
 
+  const changeTextPostHandler = () => {
+    if (newPostElement.current) {
+      props.updateTextPostsMessage(newPostElement.current.value)
+    }
+  }
 
   return (
     <div className={s.posts}>
       <h3>My posts</h3>
       <div className={s.inputArea}>
-        <textarea ref={newPostElement} placeholder={' Расскажи свою историю!'}></textarea>
-        <MessageButton name={'add New Post'} callback={addNewPost}/>
+        <textarea onChange={changeTextPostHandler}
+                  ref={newPostElement}
+                  placeholder={' Расскажи свою историю!'}
+                  value={props.textForUpdate}/>
+        <MessageButton name={'add New Post'} callback={addNewPostHandler}/>
 
       </div>
       {postsElement}
