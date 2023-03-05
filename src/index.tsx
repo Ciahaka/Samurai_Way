1064a31eda1b73ed7f8d1ecc36fe0490d4e060c8
Ciahaka <wilddiver.by@gmail.com>
2023-03-05 20:57:19 +0300

    return rerender func into index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,37 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import {bllSetRender} from './render';
-import {rootState} from './redux/state';
+import {App} from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import {
+  addDialogsMessage,
+  addPostsMessage, rootState,
+  RootStateType, subscribe,
+  updateTextDialogsMessage,
+  updateTextPostsMessage
+} from './redux/state'
 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const bllSetRender = (rootState:RootStateType)=>{
+  root.render(
+    <BrowserRouter>
+      <App state={rootState}
+           textForUpdate={rootState.posts.textForUpdate}
+           messageValue={rootState.dialogs.dialogsMessageValue}
+           addPostsMessage={addPostsMessage}
+           updateTextPostsMessage={updateTextPostsMessage}
+           updateTextDialogsMessage={updateTextDialogsMessage}
+           addDialogsMessage={addDialogsMessage}
+      />
+    </BrowserRouter>
+  )
+}
 
 bllSetRender(rootState)
+subscribe(bllSetRender)
+
+reportWebVitals();
