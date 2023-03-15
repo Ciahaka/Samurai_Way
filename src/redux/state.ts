ce9f96b496347dbe9a3e3f6337f1d4bd4e28b0c4
Ciahaka <wilddiver.by@gmail.com>
2023-03-15 20:57:27 +0300

    add #37_Samurai 'Store'

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,13 +1,6 @@
 import {v1} from 'uuid';
 
 
-let bllSetRender = (state: RootStateType) => {
-
-}
-export const subscribe = (observer: (state: RootStateType) => void) => {
-  bllSetRender = observer
-}
-
 
 export type DialogDataType = {
   id: string
@@ -37,68 +30,86 @@ export type RootStateType = {
   posts: PostsType
 
 }
-export const rootState: RootStateType = {
-  dialogs: {
-    dialogsData: [
-      {id: v1(), name: 'Oleg'},
-      {id: v1(), name: 'Turbo'},
-      {id: v1(), name: 'Morgan'},
-      {id: v1(), name: 'Vadim'},
-      {id: v1(), name: 'Алексей'},
-      {id: v1(), name: 'ХАН'},
-    ],
-    messageData: [
-      {id: v1(), message: 'Привет! Рады видеть тебя здесь!'},
-      {id: v1(), message: 'Спасибо за приглашение!'},
-      {id: v1(), message: 'Hi bro'},
-    ],
-    dialogsMessageValue: ''
+export type StoreType = {
+  _rootState: RootStateType
+  addPostsMessage: () => void
+  updateTextPostsMessage: (newText: string) => void
+  addDialogsMessage: () => void
+  updateTextDialogsMessage: (newText: string) => void
+  _bllSetRender: (state: RootStateType) => void
+  subscribe: (observer: (state: RootStateType) => void) => void
+  getState: () => RootStateType
+}
+
+export const store: StoreType = {
+  _rootState: {
+    dialogs: {
+      dialogsData: [
+        {id: v1(), name: 'Oleg'},
+        {id: v1(), name: 'Turbo'},
+        {id: v1(), name: 'Morgan'},
+        {id: v1(), name: 'Vadim'},
+        {id: v1(), name: 'Алексей'},
+        {id: v1(), name: 'ХАН'},
+      ],
+      messageData: [
+        {id: v1(), message: 'Привет! Рады видеть тебя здесь!'},
+        {id: v1(), message: 'Спасибо за приглашение!'},
+        {id: v1(), message: 'Hi bro'},
+      ],
+      dialogsMessageValue: ''
+    },
+    posts: {
+      textForUpdate: '',
+      postsData: [
+        {id: v1(), message: 'Привет! Что нового?!', likesCount: 15},
+        {id: v1(), message: 'Всё отлично! Как сам?', likesCount: 3},
+        {id: v1(), message: 'В порядке! Спасибо зарядке!', likesCount: 8},
+      ]
+    },
+
   },
-  posts: {
-    textForUpdate: '',
-    postsData: [
-      {id: v1(), message: 'Привет! Что нового?!', likesCount: 15},
-      {id: v1(), message: 'Всё отлично! Как сам?', likesCount: 3},
-      {id: v1(), message: 'В порядке! Спасибо зарядке!', likesCount: 8},
-    ]
+  addPostsMessage() {
+    const newPostsMessage = {
+      id: v1(),
+      message: this._rootState.posts.textForUpdate,
+      likesCount: 145
+    };
+
+    this._rootState.posts.postsData.push(newPostsMessage)
+    this._rootState.posts.textForUpdate = ''
+    this._bllSetRender(this._rootState)
   },
+  updateTextPostsMessage(newText: string) {
 
-}
+    this._rootState.posts.textForUpdate = newText
+    this._bllSetRender(this._rootState)
+  },
+  addDialogsMessage() {
+    const newPostsMessage = {
+      id: v1(),
+      message: this._rootState.dialogs.dialogsMessageValue,
 
-export const addPostsMessage = () => {
-  const newPostsMessage = {
-    id: v1(),
-    message: rootState.posts.textForUpdate,
-    likesCount: 145
-  };
+    };
 
-  rootState.posts.postsData.push(newPostsMessage)
-  rootState.posts.textForUpdate = ''
-  bllSetRender(rootState)
-}
+    this._rootState.dialogs.messageData.push(newPostsMessage)
+    this._rootState.dialogs.dialogsMessageValue = ''
+    this._bllSetRender(this._rootState)
+  },
+  updateTextDialogsMessage(newText: string) {
 
-export const updateTextPostsMessage = (newText: string) => {
+    this._rootState.dialogs.dialogsMessageValue = newText
+    this._bllSetRender(this._rootState)
+  },
+  _bllSetRender(state: RootStateType) {
 
-  rootState.posts.textForUpdate = newText
-  bllSetRender(rootState)
-}
-
-export const addDialogsMessage = () => {
-  const newPostsMessage = {
-    id: v1(),
-    message: rootState.dialogs.dialogsMessageValue,
-
-  };
-
-  rootState.dialogs.messageData.push(newPostsMessage)
-  rootState.dialogs.dialogsMessageValue = ''
-  bllSetRender(rootState)
-}
-
-export const updateTextDialogsMessage = (newText: string) => {
-
-  rootState.dialogs.dialogsMessageValue = newText
-  bllSetRender(rootState)
+  },
+  subscribe(observer) {
+    this._bllSetRender = observer
+  },
+  getState() {
+    return this._rootState
+  }
 }
 
 
