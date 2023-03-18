2b1a239188fef94e9142b23160d639061baf1894
Ciahaka <wilddiver.by@gmail.com>
2023-03-18 22:28:57 +0300

    refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,18 @@ import {Photo} from './Component/navigation aside/photo/Photo';
 import {Contacts} from './Component/footer block/our contacts/Contacts';
 import {Project} from './Component/footer block/project/Project';
 import {AboutUs} from './Component/footer block/about us/AboutUs';
-import {RootStateType, StoreType} from './redux/state'
+import {ActionType, RootStateType, StoreType} from './redux/state'
 import {Friends} from './Component/navigation aside/friends/Friends';
 import {NewsBlock} from './Component/news/news block/NewsBlock';
 
 export type StatePropsType = {
   state: RootStateType
-  addPostsMessage: () => void
-  addDialogsMessage:()=>void
   textForUpdate: string
   messageValue:string
-  updateTextPostsMessage:(newText: string)=>void
-  updateTextDialogsMessage:(newText: string)=>void
   store:StoreType
+  dispatch:(action:ActionType)=>void
 }
+
 export const App = (props: StatePropsType) => {
 const state = props.store.getState()
 
@@ -37,21 +35,21 @@ const state = props.store.getState()
         <Routes>
           <Route path={'/Home/*'} element={<Profile postsData={state.posts.postsData}
                                                     textForUpdate={props.textForUpdate}
-                                                    addPostsMessage={props.store.addPostsMessage.bind(props.store)}
-                                                    updateTextPostsMessage={props.store.updateTextPostsMessage.bind(props.store)}
+                                                    dispatch={props.dispatch}
+
           />}/>
 
           <Route path={'/Profile/*'} element={<Profile postsData={state.posts.postsData}
                                                        textForUpdate={props.textForUpdate}
-                                                       addPostsMessage={props.store.addPostsMessage.bind(props.store)}
-                                                       updateTextPostsMessage={props.store.updateTextPostsMessage.bind(props.store)}
+                                                       dispatch={props.dispatch}
+
           />}/>
 
           <Route path={'/Dialogs/*'} element={<Dialogs dialogsData={state.dialogs.dialogsData}
                                                        messageData={state.dialogs.messageData}
                                                        messageValue={props.messageValue}
-                                                       updateTextDialogsMessage={props.store.updateTextDialogsMessage.bind(props.store)}
-                                                       addDialogsMessage={props.store.addDialogsMessage.bind(props.store)}
+                                                       dispatch={props.dispatch}
+
           />}/>
           <Route path={'/Music'} element={<Music/>}/>
           <Route path={'/Photo'} element={<Photo/>}/>
