277ff49eb6c8da633142f413759d87648ca6eb5f
Ciahaka <wilddiver.by@gmail.com>
2023-03-19 22:52:43 +0300

    add

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -52,7 +52,8 @@ export type UpdateTextDialogsActionType = {
   newText: string
 }
 
-export type ActionType = AddPostActionType
+export type ActionType =
+  AddPostActionType
   | UpdateTextPostActionType
   | AddPostDialogsType
   | UpdateTextDialogsActionType
